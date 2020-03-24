0fa874574dbffbdd5ee37c882d65e1b6cc5af86b
commander <ashishsheth@gmail.com>
2020-03-24 19:45:01 +0530

    Added DFS for directed graph

---
+++ Graph/Digraph.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Graph
+{
+    public class Digraph
+    {
+        public int V
+        {
+            get;
+            private set;
+        }
+
+        public int E
+        {
+            get;
+            private set;
+        }
+
+        private HashSet<int>[] adjacencyLists;
+
+        public Digraph(int v)
+        {
+            this.V = v;
+            this.E = 0;
+            this.adjacencyLists = new HashSet<int>[V];
+            for(int i = 0; i < this.V; i++)
+            {
+                this.adjacencyLists[i] = new HashSet<int>();
+            }
+        }
+
+        public Digraph(int v, List<int[]> edges) : this(v)
+        {
+            this.E = edges.Count;
+
+            foreach (int[] edge in edges)
+            {
+                AddEdge(edge[0], edge[1]);
+            }
+        }
+
+        public IEnumerable<int> GetAdjacencyList(int v)
+        {
+            return this.adjacencyLists[v];
+        }
+
+        public void AddEdge(int from, int to)
+        {
+            if(this.adjacencyLists[from] == null)
+            {
+                this.adjacencyLists[from] = new HashSet<int>();
+            }
+
+            this.adjacencyLists[from].Add(to);
+        }
+
+        public Digraph Reverse()
+        {
+            Digraph r = new Digraph(this.V);
+            for(int v = 0; v < this.V; v++)
+            {
+                foreach(int w in this.GetAdjacencyList(v))
+                {
+                    r.AddEdge(w, v);
+                }
+            }
+
+            return r;
+        }
+    }
+}
---
+++ Graph/DirectedDepthFirstSearch.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Graph
+{
+    public class DirectedDepthFirstSearch
+    {
+        private Boolean[] marked;
+
+        public DirectedDepthFirstSearch(Digraph g, int s)
+        {
+            this.marked = new bool[g.V];
+            Dfs(g, s);
+        }
+
+        public DirectedDepthFirstSearch(Digraph g, List<int> sources)
+        {
+            this.marked = new bool[g.V];
+            foreach(int s in sources)
+            {
+                if (!marked[s])
+                {
+                    Dfs(g, s);
+                }
+            }
+            
+        }
+
+        public bool Marked(int v)
+        {
+            return this.marked[v];
+        }
+
+        private void Dfs(Digraph g, int s)
+        {
+            this.marked[s] = true;
+
+            foreach(int v in g.GetAdjacencyList(s))
+            {
+                if (!marked[v])
+                {
+                    Dfs(g, v);
+                }
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            string fileName = args[0];
+            string[] lines = File.ReadAllLines(fileName);
+
+            int v = int.Parse(lines[0]);
+            int e = int.Parse(lines[1]);
+
+            List<int[]> edges = new List<int[]>();
+
+            for(int i = 2; i < lines.Length; i++)
+            {
+                string[] points = lines[i].Split(' ');
+                edges.Add(new int[] { int.Parse(points[0]), int.Parse(points[1]) });
+            }
+
+            Digraph dg = new Digraph(v, edges);
+
+            List<int> sources = new List<int>();
+
+            for(int i = 1; i < args.Length; i++)
+            {
+                sources.Add(int.Parse(args[i]));
+            }
+
+            DirectedDepthFirstSearch ddfs = new DirectedDepthFirstSearch(dg, sources);
+            for(int ve = 0; ve < dg.V; ve++)
+            {
+                if (ddfs.Marked(ve))
+                {
+                    Console.Write(ve + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
